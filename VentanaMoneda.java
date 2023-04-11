d17a6d24f696943bbbc1812d52e6ee3c9602928a
Cesar Zalamea <czalamea115@outlook.com>
2023-04-11 12:28:07 -0500

    se termina la interfaz grafica

--- VentanaMoneda.java
+++ VentanaMoneda.java
@@ -6,7 +6,7 @@ public class VentanaMoneda extends JFrame {
     public VentanaMoneda(){
         //setSize(500, 500);
         setTitle("Conversor de moneda"); //titulo
-        setBounds(250,250,900,900); //tamanio y posicion inicial de la ventana
+        setBounds(250,250,500,500); //tamanio y posicion inicial de la ventana
         setLocationRelativeTo(null); //ventana en el centro
         iniciarComponentes();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -18,6 +18,7 @@ public class VentanaMoneda extends JFrame {
         colocarEtiquetas();
         colocarTextField();
         colocarComboBox();
+        colocarBotones();
 
     }
 
@@ -40,7 +41,25 @@ public class VentanaMoneda extends JFrame {
         etiqueta.setBounds(10,10, 200 ,50);
         etiqueta.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 
+        JLabel etiqueta2 = new JLabel();
+        etiqueta2.setText("Ingrese el valor a convertir en x");
+        etiqueta2.setHorizontalAlignment(JLabel.CENTER);
+        etiqueta2.setVerticalAlignment(JLabel.CENTER);
+
+        etiqueta2.setBounds(0,150, 300 ,50);
+
+        JLabel etiqueta3 = new JLabel();
+        etiqueta3.setText("El resultado de x a y es: ");
+        etiqueta3.setHorizontalAlignment(JLabel.CENTER);
+        etiqueta3.setVerticalAlignment(JLabel.CENTER);
+
+        etiqueta3.setBounds(0,350, 300 ,50);
+
+
+
         panel.add(etiqueta);
+        panel.add(etiqueta2);
+        panel.add(etiqueta3);
     }
 
     private void colocarComboBox (){
@@ -48,7 +67,7 @@ public class VentanaMoneda extends JFrame {
         String [] opciones = {"De dolares a pesos colombianos", "De dolares a soles","De dolares a pesos argentinos", "De dolares a bolivares (Venezuela)", "De dolares a Euros", "De dolares a Yen", "De dolares a Libras", "De dolares a Yenes"};
 
         JComboBox conversiones = new JComboBox<>(opciones);
-        conversiones.setBounds(50,100,200,25);
+        conversiones.setBounds(50,100,250,30);
         panel.add(conversiones);
 
 
@@ -59,11 +78,24 @@ public class VentanaMoneda extends JFrame {
         JTextField texto1 = new JTextField();
         //JTextField texto2 = new JTextField();
         //JTextField texto3 = new JTextField();
-
         texto1.setBounds(50, 200, 200,30);
+
+        JTextField texto2 = new JTextField();
+        texto2.setBounds(50,400, 200,30);
+
+
         panel.add(texto1);
+        panel.add(texto2);
     }
 
+    private void colocarBotones (){
 
+        JButton boton = new JButton();
+        boton.setBounds(350,50,100,40);
+        ImageIcon alura = new ImageIcon("alura boton.png");
+        boton.setIcon(new ImageIcon(alura.getImage().getScaledInstance(boton.getWidth(),boton.getHeight(), Image.SCALE_SMOOTH )));
+
+        panel.add(boton);
+    }
 
 }
